541c9b6aec2b989a8ba67d76ac6629d089574b76
Tyron <tyronsamaroo@gmail.com>
2019-12-06 02:28:51 -0500

    Create a React app and publish it to GitHub Pages

---
+++ client/build/precache-manifest.13660e3a64e0484b77e5a9ec9bdc7cf2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e569d633b8e8ffe190885798511344e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d2221866e24215e3892",
+    "url": "/static/css/main.93cbfddf.chunk.css"
+  },
+  {
+    "revision": "aaaa19c1ba1f68121cac",
+    "url": "/static/js/2.287a2173.chunk.js"
+  },
+  {
+    "revision": "9d2221866e24215e3892",
+    "url": "/static/js/main.2e684b9a.chunk.js"
+  },
+  {
+    "revision": "131a15b720a3f2bd598a",
+    "url": "/static/js/runtime-main.8c478e4e.js"
+  },
+  {
+    "revision": "b0537f86f8721dc4334fc9c15256ec8c",
+    "url": "/static/media/AboutHero.b0537f86.svg"
+  },
+  {
+    "revision": "e45bf5ce823535edb3a7cf0257a96dc9",
+    "url": "/static/media/Breakfast.e45bf5ce.jpg"
+  },
+  {
+    "revision": "ac9ba0986dc0fccaf9989910c596564f",
+    "url": "/static/media/DayanProfile.ac9ba098.png"
+  },
+  {
+    "revision": "fa6cd128aeb36e04f23140194627736b",
+    "url": "/static/media/EricProfile.fa6cd128.jpg"
+  },
+  {
+    "revision": "7df033cbcffefab46d0b4d71dac6b03b",
+    "url": "/static/media/Logo.7df033cb.svg"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "3b6483ffc9ecd11f4df796b2e01911ed",
+    "url": "/static/media/TyronProfile.3b6483ff.png"
+  },
+  {
+    "revision": "7d036cf8793eb4a36d015f1426cd68ff",
+    "url": "/static/media/breakfastImage.7d036cf8.jpg"
+  },
+  {
+    "revision": "508beb8c7f6474b0a1d0d0dea77fab9d",
+    "url": "/static/media/building_blocks.508beb8c.svg"
+  },
+  {
+    "revision": "a72bd0747a52cc0e85ae752883d9cd6a",
+    "url": "/static/media/burgerImage.a72bd074.jpg"
+  },
+  {
+    "revision": "a1afb525695d3b3b27ebc237e722085c",
+    "url": "/static/media/dessertImage.a1afb525.jpg"
+  },
+  {
+    "revision": "81f42d4f1835a03090dd3724865a46d7",
+    "url": "/static/media/healthyImage.81f42d4f.jpg"
+  },
+  {
+    "revision": "440a9d09bceec93e6bc3e96e8cdc3479",
+    "url": "/static/media/mexicanImage.440a9d09.jpg"
+  },
+  {
+    "revision": "8f24c54bb489055a4f35b2b0a9298777",
+    "url": "/static/media/undraw_searching_p5ux.8f24c54b.png"
+  }
+]);
